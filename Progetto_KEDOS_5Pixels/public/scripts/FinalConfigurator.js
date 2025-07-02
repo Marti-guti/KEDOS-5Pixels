771ba0191a721f85df1f1a81b6a05f5a4d6569fa
Vittorio Cenni <Vittorio.Cenni@edu-its.it>
2025-07-02 17:57:48 +0200

    aggiunte funzioni principali per far comparire le carte prossimo passo aggiungere l'evento per il change

--- Progetto_KEDOS_5Pixels/public/scripts/FinalConfigurator.js
+++ Progetto_KEDOS_5Pixels/public/scripts/FinalConfigurator.js
@@ -1,7 +1,9 @@
 let gettingLangFromHome = new URLSearchParams(window.location.search);
-let currentLang = gettingLangFromHome.get("lang");
+let currentLang = gettingLangFromHome.get("lang") || "en";
 console.log(currentLang);
 const progressBarNodes = document.querySelectorAll('.progressBarNode');
+
+let currentPage = "setUp";
 
 const langPages = {
   en: document.getElementById("en-page"),
@@ -14,24 +16,18 @@ const pages = {
     duration: document.getElementById("duration-En"),
     classNumber: document.getElementById("classNumber-En"),
     form: document.getElementById("form-En"),
-    nextBtn: document.getElementById("nextBtnEn"),
-    summary: document.getElementById("summaryListEn")
   },
   es: {
     setUp: document.getElementById("setUp-Es"),
     duration: document.getElementById("duration-Es"),
     classNumber: document.getElementById("classNumber-Es"),
     form: document.getElementById("form-Es"),
-    nextBtn: document.getElementById("nextBtnEs"),
-    summary: document.getElementById("summaryListEs")
   },
   it: {
     setUp: document.getElementById("setUp-It"),
     duration: document.getElementById("duration-It"),
     classNumber: document.getElementById("classNumber-It"),
     form: document.getElementById("form-It"),
-    nextBtn: document.getElementById("nextBtnIt"),
-    summary: document.getElementById("summaryListIt")
   }
 };
 
@@ -41,3 +37,33 @@ const nextBtnS = {
     nextBtnIt: document.getElementById("")
 };
 
+function removeOtherLangPages(){
+  for (let language in langPages){
+      if (language === currentLang){
+          langPages[language].classList.remove("d-none");
+      }
+      else {
+          langPages[language].classList.add("d-none");
+      }
+  }
+};
+
+function removeOtherPages(){
+  for (let language in pages){
+    if (language === currentLang){
+      for (let page in pages[language]){
+        const element = pages[language][page];
+        if (page === currentPage){
+          element.classList.remove("d-none");
+        } else {
+          element.classList.add("d-none");
+        }
+      }
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  removeOtherLangPages(); 
+  removeOtherPages();
+});
